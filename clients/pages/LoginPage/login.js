955f78ffa57d8bb471e0681dce8f258eff94b63e
NelsenJ <nelsen.johnathan@gmail.com>
2023-11-25 09:31:58 +0700

    responsive for pc's and bigger screen

--- clients/pages/LoginPage/login.js
+++ clients/pages/LoginPage/login.js
@@ -1,3 +1,17 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    const language = document.querySelector("#language");
-})
+document.addEventListener("DOMContentLoaded", () => {
+    var navbar = document.querySelector("#nav");
+    var header = document.querySelector("header")
+    var sticky = navbar.offsetTop;
+
+    window.onscroll = function() {
+        myFunction();
+    };
+
+    function myFunction() {
+        if (window.pageYOffset >= sticky) {
+            header.classList.add("fixed");
+        } else {
+            header.classList.remove("fixed");
+        }
+    }
+});
