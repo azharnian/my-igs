7196b6a582a37a2d68f715112abbd4b62e1cd5d0
syacia09 <keisya.valencia03@gmail.com>
2023-11-28 20:27:25 +0700

    Hide Nav (msh kurang rapi)

--- clients/pages/LoginPage/login.js
+++ clients/pages/LoginPage/login.js
@@ -1,28 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-    var navbar = document.querySelector("#nav");
-    var header = document.querySelector("header")
-    var sticky = navbar.offsetTop;
-    //nav background color
-    window.onscroll = function() {
-        myFunction();
-    };
+  var navbar = document.querySelector("#nav");
+  var header = document.querySelector("header");
+  var sticky = navbar.offsetTop;
+  //nav background color
+  window.onscroll = function () {
+    myFunction();
+  };
 
-    function myFunction() {
-        if (window.pageYOffset >= sticky) {
-            header.classList.add("fixed");
-        } else {
-            header.classList.remove("fixed");
-        }
+  function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      header.classList.add("fixed");
+    } else {
+      header.classList.remove("fixed");
     }
+  }
 
-    var hiddenLanguage = document.querySelector(".hidden-nav-language")
-    var language = document.querySelector(".language");
-    language.addEventListener('click', function(){
-        
-    })
-    //hidenav responsive
-    var smthng
-    //language nav
-    var smthng
+  var hiddenLanguage = document.querySelector(".hidden-nav-language");
+  var language = document.querySelector(".language");
+  language.addEventListener("click", function () {});
 
+  //hidenav responsive
+  const hamburger_menu = document.querySelector("button.hamburger");
+  const mobile_menu = document.querySelector("nav.mobile-nav");
+
+  hamburger_menu.addEventListener("click", function () {
+    hamburger_menu.classList.toggle("is-active");
+    mobile_menu.classList.toggle("is-active");
+  });
+  //language nav
+  var smthng;
 });
