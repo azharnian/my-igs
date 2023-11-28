60bd213c57520c99f2dac76d34be40783058eaa5
noxca <ahmadrizkidaffaa@gmail.com>
2023-11-28 22:19:37 +0700

    Revising users columns and attributes as well adding some of default values

--- application/users/models.py
+++ application/users/models.py
@@ -1,32 +1,29 @@
+from datetime import datetime
+
 from flask_login import UserMixin
 
 from application import db
 from application.project.models import BaseModel
 
-class Role(db.Model, BaseModel):
-
-    __tablename__ = "roles"
-    id = db.Column(db.Integer, primary_key=True)
-
 class User(db.Model, UserMixin, BaseModel):
 
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(128), nullable = False)
+    username = db.Column(db.String(128), unique=True, nullable = False)
     password = db.Column(db.String(128), nullable = False)
     first_name = db.Column(db.String(64), nullable = False)
     last_name = db.Column(db.String(64), nullable = False)
-    email = db.Column(db.String(128), nullable = False)
-    phone = db.Column(db.Integer, nullable = False)
-    date_joined = db.Column(db.DateTime, nullable = False)
-    last_ip = db.Column(db.String(15), nullable = False)
-    last_login = db.Column(db.Boolean(), nullable = False)
-    is_superuser = db.Column(db.Boolean(), nullable = False)
-    is_active = db.Column(db.Boolean(), nullable = False)
-    is_admin = db.Column(db.Boolean(), nullable = False)
-    is_staff = db.Column(db.Boolean(), nullable = False)
-    is_student = db.Column(db.Boolean(), nullable = False)
+    fullname = first_name + last_name
+    email = db.Column(db.String(128), unique=True, nullable = False)
+    phone = db.Column(db.String(16), unique=True, nullable = False)
+    date_joined = db.Column(db.DateTime, nullable = False, default = datetime.utcnow())
+    last_ip = db.Column(db.String(15), nullable = False, default = "192.168.0.1")
+    last_login = db.Column(db.DateTime, nullable = False, default = datetime.utcnow())
+    is_superuser = db.Column(db.Boolean, nullable = False, default = False)
+    is_active = db.Column(db.Boolean, nullable = False, default = False)
+    is_admin = db.Column(db.Boolean, nullable = False, default = False)
+    is_staff = db.Column(db.Boolean, nullable = False, default = False)
+    is_student = db.Column(db.Boolean, nullable = False, default = False)
 
     def __repr__(self):
-        return f"{self.id} - {self.username} - {self.first_name} {self.last_name}"
-    
+        return f"{self.id} - {self.username} - {self.fullname}"
