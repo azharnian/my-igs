f6391218ea7d195f96d9966f1f7ae46d558d5b07
Antonius-Ricky <antoniusrickyigs@gmail.com>
2023-11-29 20:58:43 +0700

    fixed the model

--- application/achievements/models.py
+++ application/achievements/models.py
@@ -7,24 +7,27 @@ class AchievementType(db.Model, BaseModel):
 
     __tablename__ = "achievement_types"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    type = db.Column(db.String(64), nullable=False)
+    category = db.Column(db.String(64), nullable=False)
     level = db.Column(db.String(64), nullable=False)
-    earned_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    description = db.Column(db.String(256))
     created_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     status = db.Column(db.Boolean, default=True)
     online = db.Column(db.Boolean, default=False)
-    achievement_type = db.relationship('AchievementType', backref='achievements')
+    
+    achievements = db.relationship('AchievementType', backref='achievements_in type')
 
 class Achievement(db.Model, BaseModel):
 
     __tablename__ = 'achievements'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    achievement = db.Column(db.String(128), nullable=False)
+    name = db.Column(db.String(128), nullable=False)
     organizer = db.Column(db.String(128), nullable=False)
     student_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     type_id = db.Column(db.Integer, db.ForeignKey('achievement_types.id'), nullable=False)
     input_by = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    description = db.Column(db.String(256))
+    comp_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) #tanggal lomba
+    start_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    end_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 
     def __repr__(self):
