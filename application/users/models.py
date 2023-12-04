2dd41332bbcaeb6bf910b7c9b52185989a9edaad
noxca <ahmadrizkidaffaa@gmail.com>
2023-12-04 11:59:47 +0700

    changing default last_ip on user model and creating users forms

--- application/users/models.py
+++ application/users/models.py
@@ -17,13 +17,15 @@ class User(db.Model, UserMixin, BaseModel):
     email = db.Column(db.String(128), unique=True, nullable = False)
     phone = db.Column(db.String(16), unique=True, nullable = False)
     date_joined = db.Column(db.DateTime, nullable = False, default = datetime.utcnow())
-    last_ip = db.Column(db.String(15), nullable = False, default = "192.168.0.1")
+    last_ip = db.Column(db.String(15), nullable = False, default = "127.0.0.1")
     last_login = db.Column(db.DateTime, nullable = False, default = datetime.utcnow())
     is_superuser = db.Column(db.Boolean, nullable = False, default = False)
     is_active = db.Column(db.Boolean, nullable = False, default = False)
     is_admin = db.Column(db.Boolean, nullable = False, default = False)
     is_staff = db.Column(db.Boolean, nullable = False, default = False)
     is_student = db.Column(db.Boolean, nullable = False, default = False)
+
+
 
     def __repr__(self):
         return f"{self.id} - {self.username} - {self.fullname}"
