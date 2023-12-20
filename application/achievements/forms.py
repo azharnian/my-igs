0d3cb015b556b1808076a3479597c2d12ab1753e
Anas Azhar <anasaz.har@icloud.com>
2023-12-20 14:04:05 +0700

    Fixed DateField in Form

--- application/achievements/forms.py
+++ application/achievements/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, SubmitField, TextAreaField, DateTimeField, DateField
+from wtforms import StringField, BooleanField, SubmitField, TextAreaField, DateField
 from wtforms.validators import DataRequired, Optional
 from flask_wtf.file import FileField, FileAllowed
 
@@ -9,9 +9,9 @@ class AddAchievementForm(FlaskForm):
     description = TextAreaField("Description")
     category = StringField("Category", validators=[DataRequired()])
     online = BooleanField('Online', validators=[DataRequired()], default = False)
-    comp_date = DateTimeField("Competition Date", validators=[DataRequired()])
-    start_date = DateTimeField("Start Date", validators=[DataRequired()])
-    end_date = DateTimeField("End Date", validators=[DataRequired()])
+    comp_date = DateField("Competition Date", validators=[DataRequired()])
+    start_date = DateField("Start Date", validators=[DataRequired()])
+    end_date = DateField("End Date", validators=[DataRequired()])
     submit = SubmitField("Add Achievement")
 
 class UpdateAchievementForm(AddAchievementForm):
