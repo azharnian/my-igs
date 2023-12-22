ffbb12f01b9cc3385b47fcfc336f671fb318febe
NelsenJ <nelsen.johnathan@gmail.com>
2023-12-22 22:12:42 +0700

    function darkmode created

--- clients/pages/DashboardPage/dashboard.js
+++ clients/pages/DashboardPage/dashboard.js
@@ -1,49 +1,60 @@
-const menu_btn = document.querySelector("button.hamburger");
-const mobile_menu = document.querySelector(".mobile-nav");
-const cross_menu = document.querySelector(".x");
-const logo = document.getElementById("header--logo");
-const languange = document.getElementById("header--language");
-const body = document.querySelector("body")
-menu_btn.addEventListener("click", function () {
-  body.classList.toggle("is-active")
-  menu_btn.classList.toggle("is-active");
-  mobile_menu.classList.toggle("is-active");
-});
+document.addEventListener("DOMContentLoaded", function(){
+    const menu_btn = document.querySelector("button.hamburger");
+  const mobile_menu = document.querySelector(".mobile-nav");
+  const cross_menu = document.querySelector(".x");
+  const logo = document.getElementById("header--logo");
+  const languange = document.getElementById("header--language");
+  const body = document.querySelector("body")
+  menu_btn.addEventListener("click", function () {
+    body.classList.toggle("is-active")
+    menu_btn.classList.toggle("is-active");
+    mobile_menu.classList.toggle("is-active");
+  });
 
-cross_menu.addEventListener("click", function () {
-  body.classList.toggle("is-active")
-  menu_btn.classList.remove("is-active");
-  mobile_menu.classList.remove("is-active");
-});
+  cross_menu.addEventListener("click", function () {
+    body.classList.toggle("is-active")
+    menu_btn.classList.remove("is-active");
+    mobile_menu.classList.remove("is-active");
+  });
 
-// function openAndClose() {
-//   if (this.booleanValue === true) {
-//     this.booleanValue = false;
-//     open();
-//   } else {
-//     this.booleanValue = true;
-//     close();
-//   }
-// }
-// function open() {
-//   menu_btn.classList.add("is-active");
-//   mobile_menu.classList.add("is-active");
-//   logo.style.display = "none";
-// }
-// function close() {
-//   menu_btn.classList.remove("is-active");
-//   mobile_menu.classList.remove("is-active");
-//   logo.style.display = "block";
-// }
+  // function openAndClose() {
+  //   if (this.booleanValue === true) {
+  //     this.booleanValue = false;
+  //     open();
+  //   } else {
+  //     this.booleanValue = true;
+  //     close();
+  //   }
+  // }
+  // function open() {
+  //   menu_btn.classList.add("is-active");
+  //   mobile_menu.classList.add("is-active");
+  //   logo.style.display = "none";
+  // }
+  // function close() {
+  //   menu_btn.classList.remove("is-active");
+  //   mobile_menu.classList.remove("is-active");
+  //   logo.style.display = "block";
+  // }
 
 
 
-//profile
+  //profile
 
-var ProfileBtn = document.getElementById("headerProf")
-var arrow = document.getElementById("arrow")
-var Profilenav = document.getElementById("profNav")
-ProfileBtn.addEventListener("click", ()=>{
-    arrow.classList.toggle("show")
-    Profilenav.classList.toggle("show")
+  const ProfileBtn = document.getElementById("headerProf")
+  const arrow = document.getElementById("arrow")
+  const Profilenav = document.getElementById("profNav")
+  ProfileBtn.addEventListener("click", ()=>{
+      arrow.classList.toggle("show")
+      Profilenav.classList.toggle("show")
+  })
+
+  //darkmode
+  const buttonDark = document.getElementById("switch");
+
+  buttonDark.addEventListener('click', ()=>{
+    body.classList.toggle("dark")
+  })
+
 })
+
