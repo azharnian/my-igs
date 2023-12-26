d520eb4a8be6a5deee17701556cb79b68f0e7377
syacia09 <keisya.valencia03@gmail.com>
2023-12-26 19:37:50 +0700

    Progress Hide Nav

--- clients/pages/DashboardPage/dashboard.js
+++ clients/pages/DashboardPage/dashboard.js
@@ -1,112 +1,97 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const menu_btn = document.querySelector("button.hamburger");
+document.addEventListener("DOMContentLoaded", function () {
+  const menu_btn = document.querySelector("button.hamburger");
   const mobile_menu = document.querySelector(".mobile-nav");
   const cross_menu = document.querySelector(".x");
   const logo = document.getElementById("header--logo");
   const languange = document.getElementById("header--language");
-  const body = document.querySelector("body")
+  const body = document.querySelector("body");
+  const languageMobileNavBtn = document.getElementById("languageMobileNavBtn");
+  const languageMobileNav = document.getElementById("languageMobileNavWrapper");
+  const languageMobileWrap = document.getElementById("languageMobileWrap");
+  const arrow2 = document.getElementById("arrow2");
   menu_btn.addEventListener("click", function () {
-    body.classList.toggle("is-active")
+    body.classList.toggle("is-active");
     menu_btn.classList.toggle("is-active");
     mobile_menu.classList.toggle("is-active");
+    arrow.classList.toggle("show");
   });
 
   cross_menu.addEventListener("click", function () {
-    body.classList.toggle("is-active")
+    body.classList.toggle("is-active");
     menu_btn.classList.remove("is-active");
     mobile_menu.classList.remove("is-active");
   });
 
-  // function openAndClose() {
-  //   if (this.booleanValue === true) {
-  //     this.booleanValue = false;
-  //     open();
-  //   } else {
-  //     this.booleanValue = true;
-  //     close();
-  //   }
-  // }
-  // function open() {
-  //   menu_btn.classList.add("is-active");
-  //   mobile_menu.classList.add("is-active");
-  //   logo.style.display = "none";
-  // }
-  // function close() {
-  //   menu_btn.classList.remove("is-active");
-  //   mobile_menu.classList.remove("is-active");
-  //   logo.style.display = "block";
-  // }
-
-
+  languageMobileNavBtn.addEventListener("click", function () {
+    languageMobileNav.classList.toggle("show");
+    arrow2.classList.toggle("show");
+  });
 
   //profile
 
-  const ProfileBtn = document.getElementById("headerProf")
-  const arrow = document.getElementById("arrow")
-  const Profilenav = document.getElementById("profNav")
-  ProfileBtn.addEventListener("click", (e)=>{
-    e.stopPropagation()
-    arrow.classList.toggle("show")
-    Profilenav.classList.toggle("show")
-  })
-  Profilenav.addEventListener('click', function(e){
-    e.stopPropagation()
-  })
-  body.addEventListener('click', function(){
-    arrow.classList.remove("show")
-    Profilenav.classList.remove("show")
-  })
+  const ProfileBtn = document.getElementById("headerProf");
+  const arrow = document.getElementById("arrow");
+  const Profilenav = document.getElementById("profNav");
+  ProfileBtn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    arrow.classList.toggle("show");
+    Profilenav.classList.toggle("show");
+  });
+  Profilenav.addEventListener("click", function (e) {
+    e.stopPropagation();
+  });
+  body.addEventListener("click", function () {
+    arrow.classList.remove("show");
+    Profilenav.classList.remove("show");
+  });
 
   //darkmode
   const buttonDark = document.getElementById("switch");
 
-  buttonDark.addEventListener('click', ()=>{
-    body.classList.toggle("dark")
-  })
+  buttonDark.addEventListener("click", () => {
+    body.classList.toggle("dark");
+  });
   //language
   const languageNav = document.getElementById("languageNavWrapper");
   const languageNavBtn = document.getElementById("languageNavBtn");
-  const chevronLanguage = document.getElementById("arrowlang")
+  const chevronLanguage = document.getElementById("arrowlang");
 
-  languageNavBtn.addEventListener('click',()=>{
+  languageNavBtn.addEventListener("click", () => {
     languageNav.classList.toggle("show");
     chevronLanguage.classList.toggle("show");
     languageNavBtn.classList.toggle("show");
-  } )
+  });
 
- //checkbox base view
- const checkboxF = document.getElementById('Indonesia');
- const checkboxS = document.getElementById('English');
+  //checkbox base view
+  const checkboxF = document.getElementById("Indonesia");
+  const checkboxS = document.getElementById("English");
 
- checkboxF.checked = true;
- let previousStateF = true;
- let previousStateS = false;
- checkboxF.addEventListener('change', function () {
-   if (checkboxF.checked) {
-      checkboxS.checked = false;  
-     previousStateF = true;
-     previousStateS = false;
-   }
-   else if (!checkboxF.checked && !checkboxS.checked){
-     checkboxF.checked = previousStateF;
-     checkboxS.checked = previousStateS;
-     previousStateF = previousStateF;
-     previousStateS = previousStateS;
-   }
- });
+  checkboxF.checked = true;
+  let previousStateF = true;
+  let previousStateS = false;
+  checkboxF.addEventListener("change", function () {
+    if (checkboxF.checked) {
+      checkboxS.checked = false;
+      previousStateF = true;
+      previousStateS = false;
+    } else if (!checkboxF.checked && !checkboxS.checked) {
+      checkboxF.checked = previousStateF;
+      checkboxS.checked = previousStateS;
+      previousStateF = previousStateF;
+      previousStateS = previousStateS;
+    }
+  });
 
- checkboxS.addEventListener('change', function () {
-   if (checkboxS.checked) {
-     checkboxF.checked = false;  
-     previousStateS = true;
-     previousStateF = false;
-   }
-   else if (!checkboxF.checked && !checkboxS.checked){
-     checkboxF.checked = previousStateF;
-     checkboxS.checked = previousStateS;
-     previousStateF = previousStateF;
-     previousStateS = previousStateS;
-   }
- });
+  checkboxS.addEventListener("change", function () {
+    if (checkboxS.checked) {
+      checkboxF.checked = false;
+      previousStateS = true;
+      previousStateF = false;
+    } else if (!checkboxF.checked && !checkboxS.checked) {
+      checkboxF.checked = previousStateF;
+      checkboxS.checked = previousStateS;
+      previousStateF = previousStateF;
+      previousStateS = previousStateS;
+    }
+  });
 });
-
