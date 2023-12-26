7de6db80e1d2899533ef33a82cb539bde1cb0c26
noxca <ahmadrizkidaffaa@gmail.com>
2023-12-26 14:11:02 +0700

    Adding resources for users

--- application/users/resources.py
+++ application/users/resources.py
@@ -1,50 +1,79 @@
+import logging
+
 from application import db
+from application.project.utils import log_activity
 from application.users.models import *
 
 def create_user(user_data):
-    new_user = User(**user_data)
-    db.session.add(new_user)
-    db.session.commit()
-    return "User account has been created successfully"
+    try:
+        user = User(
+            email = user_data['email'],
+            phone = user_data['phone'],
+            first_name = user_data['first_name'],
+            last_name = user_data['last_name'],
+            username = user_data['username'],
+            password = user_data['password']
+        )
+    except Exception as e:
+        logging.error(f"{str(e)}")
+        raise e
+    
+    user.save()
+    return {'success' : True}
 
 def get_all_users():
-    return User.query.all()
+    try:
+        ress = User.query.all()
+    except Exception as e:
+        logging.error(f"{str(e)}")
+        raise e
+    
+    return ress
 
 def get_user_by_username(user_username):
-    return User.query.filter_by(username=user_username).first()
+    try:
+        ress = User.query.filter_by(username=user_username).first()
+    except Exception as e:
+        logging.error(f"{str(e)}")
+        raise e
+    
+    return ress
 
 def get_user_by_email(user_email):
-    return User.query.filter_by(email=user_email).first()
+    try:
+        ress = User.query.filter_by(email=user_email).first()
+    except Exception as e:
+        logging.error(f"{str(e)}")
+        raise e
+    
+    return ress
 
 def update_user(user_id, new_user_data):
-    user_to_update = User.query.get(user_id)
-
-    if user_to_update:
-        for key, value in new_user_data.items():
-            setattr(new_user_data, key, value)
-        db.session.commit()
-        return "User account has been updated updated successfully"
-
-    return "User not found"
+    try:
+        user = User.query.get(int(user_id))
+        user['username'] = new_user_data['username'],
+        user['phone'] = new_user_data['phone']
+    except Exception as e:
+        logging.error(f"{str(e)}")
+        raise e
+    user.update()
+    return {'success' : True}
 
 def remove_user(user_id):
-    user_to_remove = User.query.get(user_id)
-
-    if user_to_remove:
-        db.session.delete(user_to_remove)
-        db.session.commit()
-
-        return "User account has been removed successfully"
-
-    return "User not found"
+    try:
+        user = User.query.get(int(user_id))
+    except Exception as e:
+        logging.error(f"{str(e)}")
+        raise e
+    user.remove()
+    return {'success' : True}
 
 def suspend_user(user_id):
-    user_to_disable = User.query.get(user_id)
-
-    if user_to_disable:
-        user_to_disable.is_active = False
-        db.session.commit()
-
-        return "User account has been suspended successfully"
-
-    return "User not found"
+    try:
+        user = User.query.get(int(user_id))
+    except Exception as e:
+        logging.error(f"{str(e)}")
+        raise e
+    user.is_active = False
+    user.update()
+    return {'success' : True}
