7fe17c69f938a224377eeb468d3b93484d8bd4bb
arv <marvin06ar@gmail.com>
2024-01-13 10:57:05 +0700

    certi revision

--- application/certificates/models.py
+++ application/certificates/models.py
@@ -17,13 +17,24 @@ class CertificateType(db.Model, BaseModel):
 class Certificate(db.Model, BaseModel):
 
     __tablename__ = 'certificates'
-    id            = db.Column(db.Integer, primary_key=True)
-    awardee_id    = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
-    certi_type_id = db.Column(db.Integer, db.ForeignKey("certificate_types.id"), nullable = False)
-    score         = db.Column(db.Integer, nullable = False)
-    description   = db.Column(db.String(256), nullable = False)
-    start_date    = db.Column(db.DateTime, nullable = False)
-    expired_date  = db.Column(db.DateTime, nullable = False)
+    id             = db.Column(db.Integer, primary_key=True)
+    awardee_id     = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+    certi_type_id  = db.Column(db.Integer, db.ForeignKey("certificate_types.id"), nullable = False)
+    score          = db.Column(db.Integer, nullable = False)
+    description    = db.Column(db.String(256), nullable = False)
+    start_date     = db.Column(db.DateTime, nullable = False)
+    expired_date   = db.Column(db.DateTime, nullable = False)
+
+    certi         = db.relationship("CertificateRevision", backref="revision", lazy=True)
+
 
     def __repr__(self):
         return f'{self.id} - {self.awardee} - {self.type} - {self.score}'
+    
+class CertificateRevision(db.Model, BaseModel):
+    __tablename__ = 'certificate_revisions'
+    id             = db.Column(db.Integer, primary_key=True)
+    certif_id      = db.Column(db.Integer, db.ForeignKey("certificates.id"), nullable = False)
+    created_by     = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+    created_date   = db.Column(db.DateTime, nullable = False)
+    comment        = db.Column(db.String(256), nullable = False)
