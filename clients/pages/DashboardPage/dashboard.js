3de8a7a5ed0c68470b6cb9b9091a338b318ca6b6
NelsenJ <nelsen.johnathan@gmail.com>
2024-01-24 15:56:16 +0700

    apply to achievementPage

--- clients/pages/DashboardPage/dashboard.js
+++ clients/pages/DashboardPage/dashboard.js
@@ -62,130 +62,130 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   
-  //language
-  const languageNav = document.getElementById("languageNavWrapper");
-  const languageNavBtn = document.getElementById("languageNavBtn");
-  const chevronLanguage = document.getElementById("arrowlang");
+//   //language
+//   const languageNav = document.getElementById("languageNavWrapper");
+//   const languageNavBtn = document.getElementById("languageNavBtn");
+//   const chevronLanguage = document.getElementById("arrowlang");
 
-  languageNavBtn.addEventListener("click", () => {
-    languageNav.classList.toggle("show");
-    chevronLanguage.classList.toggle("show");
-    languageNavBtn.classList.toggle("show");
-  });
+//   languageNavBtn.addEventListener("click", () => {
+//     languageNav.classList.toggle("show");
+//     chevronLanguage.classList.toggle("show");
+//     languageNavBtn.classList.toggle("show");
+//   });
 
-  //checkbox base view
-  const checkboxF = document.getElementById('Indonesia');
-  const checkboxS = document.getElementById('English');
+//   //checkbox base view
+//   const checkboxF = document.getElementById('Indonesia');
+//   const checkboxS = document.getElementById('English');
 
-  checkboxF.checked = true;
-  let previousStateF = true;
-  let previousStateS = false;
-  checkboxF.addEventListener('change', function () {
-    if (checkboxF.checked) {
-      checkboxS.checked = false;
-      checkboxFH.checked = true;
-      checkboxSH.checked = false;
-      previousStateF = true;
-      previousStateS = false;
-      previousStateFH = true;
-      previousStateSH = false;
-    }
-    else if (!checkboxF.checked && !checkboxS.checked){
-      checkboxFH.checked = previousStateFH;
-      checkboxSH.checked = previousStateSH;
-      checkboxF.checked = previousStateF;
-      checkboxS.checked = previousStateS;
-      previousStateFH = previousStateFH;
-      previousStateSH = previousStateSH;
-      previousStateF = previousStateF;
-      previousStateS = previousStateS;
-    }
-    languageNav.classList.remove("show");
-    chevronLanguage.classList.remove("show");
-    languageNavBtn.classList.remove("show");
-  });
+//   checkboxF.checked = true;
+//   let previousStateF = true;
+//   let previousStateS = false;
+//   checkboxF.addEventListener('change', function () {
+//     if (checkboxF.checked) {
+//       checkboxS.checked = false;
+//       checkboxFH.checked = true;
+//       checkboxSH.checked = false;
+//       previousStateF = true;
+//       previousStateS = false;
+//       previousStateFH = true;
+//       previousStateSH = false;
+//     }
+//     else if (!checkboxF.checked && !checkboxS.checked){
+//       checkboxFH.checked = previousStateFH;
+//       checkboxSH.checked = previousStateSH;
+//       checkboxF.checked = previousStateF;
+//       checkboxS.checked = previousStateS;
+//       previousStateFH = previousStateFH;
+//       previousStateSH = previousStateSH;
+//       previousStateF = previousStateF;
+//       previousStateS = previousStateS;
+//     }
+//     languageNav.classList.remove("show");
+//     chevronLanguage.classList.remove("show");
+//     languageNavBtn.classList.remove("show");
+//   });
 
-  checkboxS.addEventListener('change', function () {
-    if (checkboxS.checked) {
-      checkboxF.checked = false;  
-      checkboxSH.checked =true;
-      checkboxFH.checked = false;
-      previousStateS = true;
-      previousStateF = false;
-      previousStateSH = true;
-      previousStateFH = false;
-    }
-    else if (!checkboxF.checked && !checkboxS.checked){
-      checkboxFH.checked = previousStateFH;
-      checkboxSH.checked = previousStateSH;
-      checkboxF.checked = previousStateF;
-      checkboxS.checked = previousStateS;
-      previousStateFH = previousStateFH;
-      previousStateSH = previousStateSH;
-      previousStateF = previousStateF;
-      previousStateS = previousStateS;
-    }
-    languageNav.classList.remove("show");
-    chevronLanguage.classList.remove("show");
-    languageNavBtn.classList.remove("show");
-  });
-//checkbox mobile
-  const checkboxFH = document.getElementById('Indonesia2');
-  const checkboxSH = document.getElementById('English2');
+//   checkboxS.addEventListener('change', function () {
+//     if (checkboxS.checked) {
+//       checkboxF.checked = false;  
+//       checkboxSH.checked =true;
+//       checkboxFH.checked = false;
+//       previousStateS = true;
+//       previousStateF = false;
+//       previousStateSH = true;
+//       previousStateFH = false;
+//     }
+//     else if (!checkboxF.checked && !checkboxS.checked){
+//       checkboxFH.checked = previousStateFH;
+//       checkboxSH.checked = previousStateSH;
+//       checkboxF.checked = previousStateF;
+//       checkboxS.checked = previousStateS;
+//       previousStateFH = previousStateFH;
+//       previousStateSH = previousStateSH;
+//       previousStateF = previousStateF;
+//       previousStateS = previousStateS;
+//     }
+//     languageNav.classList.remove("show");
+//     chevronLanguage.classList.remove("show");
+//     languageNavBtn.classList.remove("show");
+//   });
+// //checkbox mobile
+//   const checkboxFH = document.getElementById('Indonesia2');
+//   const checkboxSH = document.getElementById('English2');
 
-  checkboxFH.checked = true;
-  let previousStateFH = true;
-  let previousStateSH = false;
-  checkboxFH.addEventListener('change', function () {
-    if (checkboxFH.checked) {
-      checkboxSH.checked = false;
-      checkboxF.checked = true;
-      checkboxS.checked = false;
-      previousStateFH = true;
-      previousStateSH = false;
-      previousStateF = true;
-      previousStateS = false;
+//   checkboxFH.checked = true;
+//   let previousStateFH = true;
+//   let previousStateSH = false;
+//   checkboxFH.addEventListener('change', function () {
+//     if (checkboxFH.checked) {
+//       checkboxSH.checked = false;
+//       checkboxF.checked = true;
+//       checkboxS.checked = false;
+//       previousStateFH = true;
+//       previousStateSH = false;
+//       previousStateF = true;
+//       previousStateS = false;
       
-    }
-    else if (!checkboxFH.checked && !checkboxSH.checked){
-      checkboxFH.checked = previousStateFH;
-      checkboxSH.checked = previousStateSH;
-      checkboxF.checked = previousStateF;
-      checkboxS.checked = previousStateS;
-      previousStateFH = previousStateFH;
-      previousStateSH = previousStateSH;
-      previousStateF = previousStateF;
-      previousStateS = previousStateS;
-    }
-    languageMobileNavBtn.classList.remove("show");
-    languageMobileNav.classList.remove("show");
-    arrow2.classList.remove("show");
-  });
+//     }
+//     else if (!checkboxFH.checked && !checkboxSH.checked){
+//       checkboxFH.checked = previousStateFH;
+//       checkboxSH.checked = previousStateSH;
+//       checkboxF.checked = previousStateF;
+//       checkboxS.checked = previousStateS;
+//       previousStateFH = previousStateFH;
+//       previousStateSH = previousStateSH;
+//       previousStateF = previousStateF;
+//       previousStateS = previousStateS;
+//     }
+//     languageMobileNavBtn.classList.remove("show");
+//     languageMobileNav.classList.remove("show");
+//     arrow2.classList.remove("show");
+//   });
 
-  checkboxSH.addEventListener('change', function () {
-    if (checkboxSH.checked) {
-      checkboxFH.checked = false;  
-      checkboxS.checked = true;
-      checkboxF.checked = false
-      previousStateSH = true;
-      previousStateFH = false;
-      previousStateS = true;
-      previousStateF = false;
-    }
-    else if (!checkboxFH.checked && !checkboxSH.checked){
-      checkboxFH.checked = previousStateFH;
-      checkboxSH.checked = previousStateSH;
-      checkboxF.checked = previousStateF;
-      checkboxS.checked = previousStateS;
-      previousStateFH = previousStateFH;
-      previousStateSH = previousStateSH;
-      previousStateF = previousStateF;
-      previousStateS = previousStateS;
-    }
-    languageMobileNavBtn.classList.remove("show");
-    languageMobileNav.classList.remove("show");
-    arrow2.classList.remove("show");
-  });
+//   checkboxSH.addEventListener('change', function () {
+//     if (checkboxSH.checked) {
+//       checkboxFH.checked = false;  
+//       checkboxS.checked = true;
+//       checkboxF.checked = false
+//       previousStateSH = true;
+//       previousStateFH = false;
+//       previousStateS = true;
+//       previousStateF = false;
+//     }
+//     else if (!checkboxFH.checked && !checkboxSH.checked){
+//       checkboxFH.checked = previousStateFH;
+//       checkboxSH.checked = previousStateSH;
+//       checkboxF.checked = previousStateF;
+//       checkboxS.checked = previousStateS;
+//       previousStateFH = previousStateFH;
+//       previousStateSH = previousStateSH;
+//       previousStateF = previousStateF;
+//       previousStateS = previousStateS;
+//     }
+//     languageMobileNavBtn.classList.remove("show");
+//     languageMobileNav.classList.remove("show");
+//     arrow2.classList.remove("show");
+//   });
 
   //notification base view
   const notifBtn = document.getElementById("notifBtn");
@@ -208,17 +208,17 @@ document.addEventListener("DOMContentLoaded", function () {
     notifBox.classList.remove("show");
   });
 
-  //languageNav mobile
+  // //languageNav mobile
 
-  const languageMobileNavBtn = document.getElementById("languageMobileNavBtn");
-  const languageMobileNav = document.getElementById("languageMobileNavWrapper");
-  const languageMobileWrap = document.getElementById("languageMobileWrap");
-  const arrow2 = document.getElementById("arrow2");
+  // const languageMobileNavBtn = document.getElementById("languageMobileNavBtn");
+  // const languageMobileNav = document.getElementById("languageMobileNavWrapper");
+  // const languageMobileWrap = document.getElementById("languageMobileWrap");
+  // const arrow2 = document.getElementById("arrow2");
 
-  languageMobileNavBtn.addEventListener("click", function () {
-    languageMobileNavBtn.classList.toggle("show");
-    languageMobileNav.classList.toggle("show");
-    arrow2.classList.toggle("show");
-  });
+  // languageMobileNavBtn.addEventListener("click", function () {
+  //   languageMobileNavBtn.classList.toggle("show");
+  //   languageMobileNav.classList.toggle("show");
+  //   arrow2.classList.toggle("show");
+  // });
 
 });
