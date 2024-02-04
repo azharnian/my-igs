21b814701ccc96e20cda8ca5f040ae7030058741
arv <marvin06ar@gmail.com>
2024-02-04 21:18:05 +0700

    Subject models - 4/2/2024

--- application/grades/models.py
+++ application/grades/models.py
@@ -1,5 +1,6 @@
 from application import db
 from application.project.models import BaseModel
+from datetime import datetime
 
 class GradeType(db.Model, BaseModel):
 
@@ -14,9 +15,13 @@ class Subject(db.Model, BaseModel):
 
     __tablename__ = 'subjects'
     id = db.Column(db.Integer, primary_key=True)
+    subject_taken = db.Column(db.String(32), nullable=False)
+    student_id    = db.Column(db.Integer, db.ForeignKey("students.id"), nullable=False)
+    assigned_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    description   = db.Column(db.String(256), nullable=False)
 
     def __repr__(self):
-        return f''
+        return f'{self.id} - {self.subject_taken}'
 
 
 class Grade(db.Model, BaseModel):
